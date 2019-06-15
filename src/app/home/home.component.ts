f7fcb8549f3dc66a8c63550e91fdbc20a44b3a3b
leonardoor90 <48846350+leonardoor90@users.noreply.github.com>
2019-06-14 21:41:57 -0300

    Atualização de fluxo de usuário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+  }
+
+  instrucao() {
+    this.snackBar.open('Bem-vindo às palestras da Fatec, confirme sua localização próxima a faculdade através da opção **CHECK-IN** do menu no canto superior esquerdo', 'OK', {duration: 20000});
   }
 
 }
--- src/app/services/geoloc.service.spec.ts
+++ src/app/services/geoloc.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GeolocService } from './geoloc.service';
+import { GeoLocService } from './geoloc.service';
 
 describe('GeolocService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: GeolocService = TestBed.get(GeolocService);
+    const service: GeoLocService = TestBed.get(GeoLocService);
     expect(service).toBeTruthy();
   });
 });
